f7ddf4abe139c5f4a2e766d4f5e3ddab7576a17c
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-04-16 15:35:53 +0100

    updateInterns

---
+++ updateInterns.js
@@ -0,0 +1,26 @@
+var mongo = require('mongodb')
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/bhenjameen";
+
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+    if (err) throw err;
+
+    var dbo = db.db('bhenjameen');
+
+    var oldUpdate = {movie: "The Banker", year: "2020", rating: 8};
+
+    var newUpdate = {$set: {movie: "Bleeding Steel", year: "2017", rating: 5.2}};
+
+    dbo.collection('myMovies').updateOne(oldUpdate, newUpdate,function(err, res) {
+    if (err) throw err;
+
+    dbo.collection('myMovies').find({}).toArray(function(err, res) {
+        if (err) throw err;
+
+            console.log(res);
+            db.close();
+        });
+    });
+});
